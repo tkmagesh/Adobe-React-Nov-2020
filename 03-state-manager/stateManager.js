4a971953c6991a221bf80c362ff4fbe410899498
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-25 11:27:12 +0530

    state manager - updates

--- 03-state-manager/stateManager.js
+++ 03-state-manager/stateManager.js
@@ -1,7 +1,8 @@
 const StateManager = (function(){
     let _currentState = undefined,
         _callbacks = [],
-        _reducer = undefined;
+        _reducer = undefined,
+        _init_action = { type : '@@INIT-ACTION' };
 
     function getState(){
         return _currentState;
@@ -28,6 +29,8 @@ const StateManager = (function(){
         if (typeof reducer !== 'function')
             throw new Error('reducer has to be a function');
         _reducer = reducer;
+        //to initialize the current state with the valid default state
+        _currentState = _reducer(_currentState /* undefined */, _init_action);
         const store = { 
             getState : getState,  
             subscribe : subscribe, 
