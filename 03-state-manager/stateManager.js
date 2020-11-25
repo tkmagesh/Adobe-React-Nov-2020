e9e1254e26c07d1930324f17007576c7f232d1e6
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-25 12:55:57 +0530

    Generalizing the refactoring the 'bindActionCreators'

--- 03-state-manager/stateManager.js
+++ 03-state-manager/stateManager.js
@@ -39,5 +39,16 @@ const StateManager = (function(){
         return store;
     }
 
-    return { createStore };
+    function bindActionCreators(actionCreators, dispatch) {
+        const actionDispatchers = {};
+        for (let key in actionCreators) {
+            actionDispatchers[key] = function (...args) {
+                const action = actionCreators[key](...args);
+                dispatch(action);
+            }
+        }
+        return actionDispatchers;
+    }
+
+    return { createStore, bindActionCreators };
 })();
